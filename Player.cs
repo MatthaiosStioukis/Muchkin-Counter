704891691f56953cfdc0094a3767ebba42b7c7c3
MatthaiosStioukis <stioukimattigr@gmail.com>
2023-01-08 00:32:25 +0200

    having issues with the creating players

--- Player.cs
+++ Player.cs
@@ -10,5 +10,25 @@ namespace MunchkinCounter
         public int level;
         public int gear;
 
+
+        
     }
+
+    public class Create 
+    {
+        public Player CreatePlayer(string name, string gender, int level, int gear) {
+
+          Player newPlayer = new Player();
+          newPlayer.name = name;
+          newPlayer.gender = gender;
+          newPlayer.level = level;
+          newPlayer.gear = gear;
+          return newPlayer;
+    }
+
+    }
+
+    
 }
+
+
